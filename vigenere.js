043dd8d17bbabe87ad8f8876940010c62916c54d
ミカエル <76715124+Mykaell-Max@users.noreply.github.com>
2021-02-02 20:54:17 -0300

    Update vigenere.js

--- vigenere.js
+++ vigenere.js
@@ -1,11 +1,24 @@
 function vigenere(phrase, key) {
     let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let cript = ''
+    let crypt = ''
     for(let n = 0; n < phrase.length; n++) {
         let p = n % key.length
         let nl = (alph.indexOf(phrase[n]) + alph.indexOf(key[p])) % 26
-        cript += alph[nl]
+        crypt += alph[nl]
     }
-    return cript
+    return crypt
+}
+
+
+function reverse_vigenere(encrypted, key) {
+    let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let decrypt = ''
+    for(let n = 0; n < encrypted.length; n++) {
+        let p = n % key.length
+        let nl = (alph.indexOf(encrypted[n]) - alph.indexOf(key[p]) + 26) % 26
+        decrypt += alph[nl]
+    }
+    return decrypt
 }
