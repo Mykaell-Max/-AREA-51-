84e4dd0c23d22e3d61e106110db3f98195eb0ce4
ミカエル <76715124+Mykaell-Max@users.noreply.github.com>
2021-02-02 20:48:13 -0300

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,9 +1,20 @@
 def vigenere(phrase, key):
     alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    cript = ''
+    crypt = ''
     for n, l in enumerate(phrase):
         p = n % len(key)
         nl = (alph.index(l) + alph.index(key[p])) % 26
-        cript += alph[nl]
-    return cript
+        crypt += alph[nl]
+    return crypt
+
+
+def reverse_vigenere(encrypted, key):
+    alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    decrypt = ''
+    for n, l in enumerate(encrypted):
+        p = n % len(key)
+        nl = (alph.index(l) - alph.index(key[p]) + 26) % 26
+        decrypt += alph[nl]
+    return decrypt
